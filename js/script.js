a850f8bceb0bcf24c04cad928ec1853a8834502f
Нина Волчек <mysha76@mail.ru>
2020-07-07 22:20:51 +0300

    Сделано открытие/закрытие окна подачи объявления

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
+const modalAdd = document.querySelector('.modal__add'),
+  addAd = document.querySelector('.add__ad'),
+  modalBtnSubmit = document.querySelector('.modal__btn-submit'),
+  modalSubmit = document.querySelector('.modal__submit');
 
+// Открытие и закрытие модального окна подачи объявления
+addAd.addEventListener('click', () => {
+  modalAdd.classList.remove('hide');
 
-  
+  // Блокировка кнопки отправки
+  modalBtnSubmit.disabled = true;
+});
+
+modalAdd.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.classList.contains('modal__close') || target === modalAdd) {
+    modalAdd.classList.add('hide');
+
+    // Очистка формы перед закрытием модального окна
+    modalSubmit.reset();
+  }
+
 
 });
+
+
+
